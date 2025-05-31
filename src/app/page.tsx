3fff17ccca5763e4745eb4ee5f932f6e6590ad99
mauriciogc <mauriciogc@gmail.com>
2025-05-31 15:25:51 -0600

    chore: remove files

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex justify-center items-center min-h-screen">
-      <h1 className="text-3xl font-bold">Página principal</h1>
-    </main>
-  );
-}

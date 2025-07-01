6c313c5e2e6a43c0dfd2f24838ec9b54fb2a1748
mauriciogc <mauriciogc@gmail.com>
2025-07-01 16:35:13 -0600

    docs: te readme is updated

--- README.md
+++ README.md
@@ -34,8 +34,19 @@ Además, se agregaron estilos base con `@layer base` y componentes reutilizables
 ### Página simple
 
 ```html
-<main className="container">
+<main className="container gap-3">
   <h1 className="title">Página principal</h1>
+  <h2 className="subTitle">Lorem ipsum dolor sit amet consectetur.</h2>
+  <p>
+    Maiores pariatur iste reprehenderit iure. Modi vero, rem animi aliquam,
+    distinctio tempora autem quae recusandae beatae debitis blanditiis quasi,
+    esse quia totam.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet
+    <span className="text-(--color-highlight)"> consectetur</span>, adipisicing
+    elit.
+  </p>
 </main>
 ```
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function HomePage() {
   return (
-    <main className="container">
+    <main className="container gap-3">
       <h1 className="title">Página principal</h1>
+      <h2 className="subTitle">Lorem ipsum dolor sit amet consectetur.</h2>
+      <p>
+        Maiores pariatur iste reprehenderit iure. Modi vero, rem animi aliquam,
+        distinctio tempora autem quae recusandae beatae debitis blanditiis
+        quasi, esse quia totam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet
+        <span className="text-(--color-highlight)"> consectetur</span>,
+        adipisicing elit.
+      </p>
     </main>
   );
 }
